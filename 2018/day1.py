b595f02b6da4045d316b201a4fcb04d4dbb1cc4c
Russell <41223103+lichen911@users.noreply.github.com>
2023-05-05 09:17:06 -0400

    Fixed merge

--- 2018/day1.py
+++ 2018/day1.py
@@ -1,29 +1,4 @@
 input_file = 'day1_input.txt'
-<<<<<<< HEAD
-# input_file = 'day1_input_test.txt'
-
-total = 0
-freq_counts = {}
-stop_loop = False
-
-while not stop_loop:
-    with open(input_file, 'r') as fd:
-        for number in fd:
-            print(number)
-            number = int(number)
-            total += number
-            try:
-                freq_counts[total] += 1
-            except KeyError:
-                freq_counts[total] = 0
-
-            if freq_counts[total] >= 1:
-                print('first dup found! - %s' % total)
-                stop_loop = True
-                break
-
-print(total)
-=======
 
 mass_list = []
 with open(input_file, 'r') as fd:
@@ -56,4 +31,3 @@ for mass in mass_list:
     fuel_sum += get_fuel_req(mass)
 
 print(fuel_sum)
->>>>>>> ad2019
