eeacfa8d0bae04e274d4699977b82cf5246f8a28
lichen911 <rhatch@gmail.com>
2023-05-07 11:55:26 -0400

    Complete day7 part 2

--- 2022/day7.go
+++ 2022/day7.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -27,13 +28,42 @@ type Dir struct {
 	Size      int
 }
 
-func (dir *Dir) calcSize() {
+func (dir *Dir) calcSizeRecurse() {
 	for _, subDir := range dir.Dirs {
+		subDir.calcSizeRecurse()
 		dir.Size += subDir.Size
 	}
 	for _, file := range dir.Files {
 		dir.Size += file.Size
 	}
+}
+
+func (dir *Dir) findDirsLessThan(size int) []*Dir {
+	var dirs []*Dir
+	if dir.Size <= size {
+		dirs = append(dirs, dir)
+	}
+	for _, subDir := range dir.Dirs {
+		dirs = append(dirs, subDir.findDirsLessThan(size)...)
+	}
+	return dirs
+}
+
+func (dir *Dir) getAllDirSizes() []int {
+	var sizes []int
+	sizes = append(sizes, dir.Size)
+	for _, subDir := range dir.Dirs {
+		sizes = append(sizes, subDir.getAllDirSizes()...)
+	}
+	return sizes
+}
+
+func sumDirs(dirs []*Dir) int {
+	var total int
+	for _, dir := range dirs {
+		total += dir.Size
+	}
+	return total
 }
 
 func main() {
@@ -46,7 +76,6 @@ func main() {
 	root := Dir{Name: "root"}
 	currentDir := &root
 	listDir := false
-	var auditDirs []*Dir
 
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -57,19 +86,17 @@ func main() {
 			cmd := strings.Split(line, " ")[1:]
 			switch cmd[0] {
 			case "ls":
+				fmt.Println("Listing dir", currentDir.Name)
 				listDir = true
 			case "cd":
 				cdArg := cmd[1]
 				if cdArg == ".." {
-					currentDir.calcSize()
-					if currentDir.Size <= 100000 {
-						auditDirs = append(auditDirs, currentDir)
-					}
-
+					fmt.Println("Going up, from", currentDir.Name, "to", currentDir.ParentDir.Name)
 					currentDir = currentDir.ParentDir
 				} else {
 					for _, dir := range currentDir.Dirs {
 						if dir.Name == cdArg {
+							fmt.Println("Going down, from", currentDir.Name, "to", dir.Name)
 							currentDir = dir
 							break
 						}
@@ -82,20 +109,44 @@ func main() {
 				dirName := strings.Split(line, " ")[1]
 				newDir := Dir{Name: dirName, ParentDir: currentDir, Size: 0}
 				currentDir.Dirs = append(currentDir.Dirs, &newDir)
+				fmt.Println("- dir", dirName)
 			} else {
 				fileSize, err := strconv.Atoi(strings.Split(line, " ")[0])
 				checkError(err)
 				fileName := strings.Split(line, " ")[1]
 				currentDir.Files = append(currentDir.Files, File{Size: fileSize, Name: fileName})
+				fmt.Println("- file", fileName, "size", fileSize)
 			}
 		} else {
 			panic("Unexpected input")
 		}
 	}
 
-	total := 0
-	for _, dir := range auditDirs {
-		total += dir.Size
+	root.calcSizeRecurse()
+
+	// Print solution to part 1
+	smallDirs := root.findDirsLessThan(100000)
+	fmt.Println("Total size of small dirs:", sumDirs(smallDirs))
+
+	// Calculate solution to part 2
+	unusedSpace := 70000000 - root.Size
+	requiredSpace := 30000000 - unusedSpace
+	var deleteDirSize int
+	var allDirSizes []int
+
+	fmt.Println("Root size:", root.Size)
+	fmt.Println("Unused space:", unusedSpace, "Required space:", requiredSpace)
+
+	allDirSizes = root.getAllDirSizes()
+	sort.Ints(allDirSizes)
+	// fmt.Println(allDirSizes)
+	fmt.Println("All dir count:", len(allDirSizes))
+
+	for _, size := range allDirSizes {
+		if size >= requiredSpace {
+			deleteDirSize = size
+			break
+		}
 	}
-	fmt.Println("Total size:", total)
+	fmt.Println("Delete dir size:", deleteDirSize)
 }
