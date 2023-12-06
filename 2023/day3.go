851ba9bc0fe35652755283e4c7bc99fda5c7fef5
Russell <41223103+lichen911@users.noreply.github.com>
2023-12-06 16:34:35 -0500

    Update day3.go

--- 2023/day3.go
+++ 2023/day3.go
@@ -37,11 +37,68 @@ func printSchematic(schematic [][]rune) {
 	}
 }
 
-func isPartNumber(schematic [][]rune, x, y, langth int) bool {
-	return true
+func isPartNumber(schematic [][]rune, x, y, length int) bool {
+	var xStart, xEnd int
+
+	// test top - y-1, x-1 to x+length+1
+	if y-1 >= 0 {
+		if x-1 < 0 {
+			xStart = 0
+		} else {
+			xStart = x-1
+		}
+
+		if x+length > len(schematic[y-1])-1 {
+			xEnd = len(schematic[y-1])-1
+		} else {
+			xEnd = x+length
+		}
+
+		for _, r := range schematic[y-1][xStart:xEnd+1] {
+			if string(r) != "." {
+				return true
+			}
+		}
+	}
+
+	// test sides - y, x-1 and x+length
+	if x-1 >= 0 {
+		if string(schematic[y][x-1]) != "." {
+			return true
+		}
+	}
+	if x+length <= len(schematic[y])-1 {
+		if string(schematic[y][x+length]) != "." {
+			return true
+		}
+	}
+
+	// test bottom y+1, x-1 to x+length+1
+	if y+1 <= len(schematic)-1 {
+		if x-1 < 0 {
+			xStart = 0
+		} else {
+			xStart = x-1
+		}
+
+		if x+length > len(schematic[y+1])-1 {
+			xEnd = len(schematic[y+1])-1
+		} else {
+			xEnd = x+length
+		}
+
+		for _, r := range schematic[y+1][xStart:xEnd+1] {
+			if string(r) != "." {
+				return true
+			}
+		}
+	}
+
+	return false
 }
 
-func findPartNumbers(schematic [][]rune) {
+
+func findPartNumbers(schematic [][]rune) int {
 	partNumSum := 0
 	for y := range schematic {
 		x := 0
@@ -65,28 +122,29 @@ func findPartNumbers(schematic [][]rune) {
 					}
 				}
 
-				fmt.Println("Part num: ", partNum)
-				x = numEnd
-			}
+				if isPartNumber(schematic, x, y, len(partNum)) {
+					partNumInt, err := strconv.Atoi(partNum)
+					checkError(err)
 
-			if isPartNumber(schematic, x, y, len(partNum)) {
-				var err error
-				partNumSum, err = strconv.Atoi(partNum)
-				checkError(err)
+					partNumSum += partNumInt
+				}
+
+				x = numEnd
 			}
 
 			x++
 		}
 	}
 
-	fmt.Println(partNumSum)
+	return partNumSum
 }
 
 func main() {
-	raw_schematic, err := os.ReadFile("day3_input_sample.txt")
+	raw_schematic, err := os.ReadFile("day3_input.txt")
 	checkError(err)
 
 	schematic := getSchematic(string(raw_schematic))
-	printSchematic(schematic)
-	findPartNumbers(schematic)
+	// printSchematic(schematic)
+	partNumSum := findPartNumbers(schematic)
+	fmt.Println("Part num sum:", partNumSum)
 }
